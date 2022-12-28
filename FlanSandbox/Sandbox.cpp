b4a1e14bc289b44714e1c9a4701014873fb9e7e1
Flan <flannyha@gmail.com>
2022-12-28 18:58:44 +0100

    started work on the plugin ui

--- FlanSandbox/Sandbox.cpp
+++ FlanSandbox/Sandbox.cpp
@@ -29,29 +29,121 @@ int main()
     //--------------------------
     // Set up components
     // Create bank text
-    Flan::Transform text_bank_transform{
-        {20, 20},
-        {100, 40},
-        0.5f,
-        Flan::AnchorPoint::top_left
-    };
-    Flan::create_text(scene, "text_bank", text_bank_transform, {
-        L"Bank:",
-        {2, 2},
-        {1, 1, 1, 1},
-        Flan::AnchorPoint::center,
-        Flan::AnchorPoint::top_left
-        });
-
+    {
+        Flan::Transform text_bank_transform{
+            {20, 20},
+            {180, 60},
+            0.5f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::create_text(scene, "text_bank", text_bank_transform, {
+            L"Bank:",
+            {2, 2},
+            {1, 1, 1, 1},
+            Flan::AnchorPoint::center,
+            Flan::AnchorPoint::center
+            });
+    }
     // Create bank numberbox
-    Flan::Transform nb_bank_transform {
-        {20, 40},
-        {100, 120},
-        0.5f,
-        Flan::AnchorPoint::top_left
-    };
-    Flan::NumberRange nb_bank_number_range{ 0, 127, 1 };
-    Flan::create_numberbox(scene, "bank", nb_bank_transform, nb_bank_number_range, 0);
+    {
+        Flan::Transform nb_bank_transform{
+            {20, 60},
+            {180, 140},
+            0.5f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::NumberRange nb_bank_number_range{ 0, 127, 1 };
+        Flan::create_numberbox(scene, "bank", nb_bank_transform, nb_bank_number_range, 0);
+    }
+    // Create bank text
+    {
+        Flan::Transform text_program_transform{
+            {180, 20},
+            {340, 60},
+            0.5f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::create_text(scene, "text_program", text_program_transform, {
+            L"Program:",
+            {2, 2},
+            {1, 1, 1, 1},
+            Flan::AnchorPoint::center,
+            Flan::AnchorPoint::center
+            });
+    }
+    // Create bank numberbox
+    {
+        Flan::Transform nb_program_transform{
+            {180, 60},
+            {340, 140},
+            0.5f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::NumberRange nb_program_number_range{ 0, 127, 1 };
+        Flan::create_numberbox(scene, "program", nb_program_transform, nb_program_number_range, 0);
+    }
+    // Create preset text
+    {
+        Flan::Transform text_preset_transform{
+            {340, 20},
+            {720, 60},
+            0.5f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::create_text(scene, "text_preset", text_preset_transform, {
+            L"Selected preset:",
+            {2, 2},
+            {1, 1, 1, 1},
+            Flan::AnchorPoint::center,
+            Flan::AnchorPoint::center
+            });
+    }
+    // Create dropdown menu for soundfont presets
+    {
+        Flan::Transform db_program_transform{
+            {340, 60},
+            {720, 140},
+            0.1f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::create_combobox(scene, "combobox_preset", db_program_transform, { L"000:000 - Piano 1", L"000:001 - Piano 2" });
+    }
+    // Create textbox for file browser
+    {
+        Flan::Transform box_soundfont_transform{
+            {20, 160},
+            {640, 240},
+            0.5f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::create_box(scene, "box_soundfont_path", box_soundfont_transform, 2.0f, {1, 1, 1, 1});
+        Flan::Transform text_soundfont_transform{
+            {30, 160},
+            {630, 240},
+            0.5f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::create_text(scene, "text_soundfont_path", text_soundfont_transform, {
+            L"C:/Windows/System32/drivers/gm.dls",
+            {2, 2},
+            {1, 1, 1, 1},
+            Flan::AnchorPoint::left,
+            Flan::AnchorPoint::left
+            });
+    }
+    // Create button for file browser
+    {
+        Flan::Transform button_soundfont_transform{
+            {640, 160},
+            {720, 240},
+            0.5f,
+            Flan::AnchorPoint::top_left
+        };
+        Flan::create_button(scene, button_soundfont_transform, []()
+            {
+                printf("hi1!\n");
+            }, { L"...", {2, 2}, {0, 0, 0, 1}, Flan::AnchorPoint::center, Flan::AnchorPoint::center });
+    }
 
     //--------------------------
 
